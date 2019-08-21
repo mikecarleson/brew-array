ce68412edcae78e68bb1481f6a45f590c1cc6f02
unknown <mikecarleson@example.com>
2019-08-20 23:58:48 -0600

    momentum scroll

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -346,16 +346,3 @@ $(".smallbutton").on('click', function(event){
 
 })
 
-$(document).ready(function() {
-    var scrollLink = $(".scroll");
-  
-    scrollLink.click(function(e) {
-      e.preventDefault();
-      $("body,html").animate(
-        {
-          scrollTop: $(this.hash).offset().top
-        },
-        1000
-      );
-    });
-  });
