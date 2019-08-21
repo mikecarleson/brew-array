0c4fe2b62c9b6e354fe597d190a60af8ae6f486d
unknown <mikecarleson@example.com>
2019-08-20 23:45:36 -0600

    mobile friendly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -345,3 +345,17 @@ $(".smallbutton").on('click', function(event){
     $("#state").val('');
 
 })
+
+$(document).ready(function() {
+    var scrollLink = $(".scroll");
+  
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $("body,html").animate(
+        {
+          scrollTop: $(this.hash).offset().top
+        },
+        1000
+      );
+    });
+  });
